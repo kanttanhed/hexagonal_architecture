72e2c777050728154bbc071d73fd3eac7a44c3b1
Plinio <plinio.salazar@yahoo.com.br>
2024-01-31 22:58:45 +0000

    feat: Creating usercase

---
+++ src/main/java/com/github/kanttanhed/hexagonal_architecture/application/core/usecase/InsertCustomerUserCase.java
@@ -0,0 +1,25 @@
+package com.github.kanttanhed.hexagonal_architecture.application.core.usecase;
+
+import com.github.kanttanhed.hexagonal_architecture.application.core.domain.Customer;
+import com.github.kanttanhed.hexagonal_architecture.application.ports.out.FindAddressByZipCodeOutputPort;
+import com.github.kanttanhed.hexagonal_architecture.application.ports.out.InsertCustomerOutputPort;
+
+public class InsertCustomerUserCase  {
+
+    private final FindAddressByZipCodeOutputPort findAddressByZipCodeOutputPort;
+
+    private final InsertCustomerOutputPort insertCustomerOutputPort;
+
+    public InsertCustomerUserCase(
+            FindAddressByZipCodeOutputPort findAddressByZipCodeOutputPort,
+            InsertCustomerOutputPort insertCustomerOutputPort) {
+        this.findAddressByZipCodeOutputPort = findAddressByZipCodeOutputPort;
+        this.insertCustomerOutputPort = insertCustomerOutputPort;
+    }
+
+    public void insert(Customer customer, String zipCode) {
+        var address = findAddressByZipCodeOutputPort.find(zipCode);
+        customer.setAddress(address);
+        insertCustomerOutputPort.insert(customer);
+    }
+}
